4bfd77bae7721d4f55a967f8ee69dcc333609be3
Sujin <tnwlsk0190@gamil.com>
2022-01-07 18:27:04 +0900

    feat: 📃글 리스트 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
 
 function App() {
+
+    const [글제목, 글제목변경] = useState(['곱창 맛집 추천','덕소','우엉차']);
+    const posts = "강남 고기 맛집";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="black-nav">
+        <div>개발 Blog</div>
+     </div>
+        <div className="list">
+            <h3> {글제목[0]} </h3>
+            <p>1월 7일 발행</p>
+            <hr/>
+        </div>
+        <div className="list">
+            <h3> {글제목[1]} </h3>
+            <p>2월 7일 발행</p>
+            <hr/>
+        </div>
+        <div className="list">
+            <h3> {글제목[2]} </h3>
+            <p>3월 7일 발행</p>
+            <hr/>
+        </div>
     </div>
   );
 }
